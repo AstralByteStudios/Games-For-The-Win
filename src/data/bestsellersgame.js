3b9230f3c8c9efc469772a4ff218fc52ebfb4aef
Joan Ramirez <fusterramirezjoan@gmail.com>
2024-03-09 01:26:58 +0100

    first commit

---
+++ src/data/bestsellersgame.js
@@ -0,0 +1,56 @@
+export const bestsellersgames = [
+  {
+    id: "minecraft",
+    name: "Minecraft",
+    img: "https://fs-prod-cdn.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/H2x1_NSwitch_Minecraft.jpg",
+    creators: "Mojang",
+  },
+  {
+    id: "grand-theft-auto",
+    name: "Grand Theft Auto V",
+    img: "https://i.f1g.fr/media/cms/orig/2023/09/19/c6cd4a76c63e71d1f1c9938b5cd3d36aa859bdd2221c48fc6a778d08b5d7c43c.jpg",
+    creators: "Rockstar Games",
+  },
+  {
+    id: "tetris",
+    name: "Tetris",
+    img: "https://m.media-amazon.com/images/I/71hV2ffEnEL.png",
+    creators: "EA",
+  },
+  {
+    id: "wii-sports",
+    name: "Wii Sports",
+    img: "https://www.nintenderos.com/wp-content/uploads/2020/04/Wii-Sports.-e1586016448965.jpg",
+    creators: "Nintendo",
+  },
+  {
+    id: "pubg",
+    name: "PUBG: BATTLEGROUNDS",
+    img: "https://i.blogs.es/212084/pubg/1366_2000.jpg",
+    creators: "PUBG Studios",
+  },
+  {
+    id: "super-mario-bros",
+    name: "Super Mario Bros.",
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2020/09/super-mario-bros-3-2065385.jpg",
+    creators: "Nintendo",
+  },
+  {
+    id: "pokemon-r-v-a-a",
+    name: "Pokémon Rojo/Verde/Azul/Amarillo",
+    img: "https://i.ytimg.com/vi/stuHcuakTgM/maxresdefault.jpg",
+    creators: "Nintendo",
+  },
+  {
+    id: "tetris-nintendo",
+    name: "Tetris (Nintendo)",
+    img: "https://i0.wp.com/lavidaesunvideojuego.com/wp-content/uploads/2019/12/Un-nuevo-modo-de-batalla-llega-a-Tetris-99-lavidaesunvideojuego.jpg?fit=1400%2C691&ssl=1",
+    creators: "Nintendo",
+  },
+  {
+    id: "pacman",
+    name: "PacMan",
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/04/pac-man-2682107.jpg?tf=3840x",
+    creators: "Nintendo",
+  },
+];
